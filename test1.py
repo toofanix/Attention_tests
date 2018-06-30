4970862817350db8c9f8e110f206c119342a8753
toofanix <jigarbandaria@gmail.com>
2018-06-30 14:08:33 -0400

    added gitignore and more util functions

--- test1.py
+++ test1.py
@@ -1,10 +1,14 @@
 from random import randint
 import numpy as np
+import typing
+
 
 def generate_sequence(length: int, n_unique: int) -> list:
 	return [randint(0, n_unique - 1) for _ in range(length)]
 
+
 sequence = generate_sequence(5, 50)
+
 
 def one_hot_encode(sequence: list, n_unique: int) -> np.array:
 	encoding = list()
@@ -14,6 +18,20 @@ def one_hot_encode(sequence: list, n_unique: int) -> np.array:
 		encoding.append(vector)
 	return np.array(encoding)
 
+
 def one_hot_decode(encoded_seq: np.array) -> list:
 	return [np.argmax(vector) for vector in encoded_seq]
 
+
+def get_pair(n_in: int, n_out: int, n_unique: int) -> typing.Any:
+	# generate random sequence
+	sequence_in = generate_sequence(n_in, n_unique)
+	sequence_out = sequence_in[:n_out] + [0 for _ in range(n_in - n_out)]
+
+	# one hot encode
+	X = one_hot_encode(sequence_in, n_unique)
+	y = one_hot_encode(sequence_out, n_unique)
+
+	# reshape as 3D
+
+	return sequence_out

dc668001c6207a0816b343305ac9dc3cdaa3aa4b
toofanix <jigarbandaria@gmail.com>
2018-07-03 20:24:03 -0400

    started attentiondecoder class

--- test1.py
+++ test1.py
@@ -10,6 +10,9 @@ from keras import regularizers, constraints, initializers, activations
 from keras.layers.recurrent import Recurrent
 
 
+def tfPrint(d, T): return tf.Print(input_=T, data=[T, tf.shape(T)], message=d)
+
+
 def _time_distributed_dense(x, w, b=None, dropout=None,
                             input_dim=None, output_dim=None,
                             timesteps=None, training=None):
@@ -56,39 +59,39 @@ def _time_distributed_dense(x, w, b=None, dropout=None,
 
 
 def generate_sequence(length: int, n_unique: int) -> list:
-	return [randint(0, n_unique - 1) for _ in range(length)]
+    return [randint(0, n_unique - 1) for _ in range(length)]
 
 
 sequence = generate_sequence(5, 50)
 
 
 def one_hot_encode(sequence: list, n_unique: int) -> np.array:
-	encoding = list()
-	for value in sequence:
-		vector = [0 for _ in range(n_unique)]
-		vector[value] = 1
-		encoding.append(vector)
-	return np.array(encoding)
+    encoding = list()
+    for value in sequence:
+        vector = [0 for _ in range(n_unique)]
+        vector[value] = 1
+        encoding.append(vector)
+    return np.array(encoding)
 
 
 def one_hot_decode(encoded_seq: np.array) -> list:
-	return [np.argmax(vector) for vector in encoded_seq]
+    return [np.argmax(vector) for vector in encoded_seq]
 
 
 def get_pair(n_in: int, n_out: int, n_unique: int) -> typing.Tuple[np.array, np.array]:
-	# generate random sequence
-	sequence_in = generate_sequence(n_in, n_unique)
-	sequence_out = sequence_in[:n_out] + [0 for _ in range(n_in - n_out)]
+    # generate random sequence
+    sequence_in = generate_sequence(n_in, n_unique)
+    sequence_out = sequence_in[:n_out] + [0 for _ in range(n_in - n_out)]
 
-	# one hot encode
-	X = one_hot_encode(sequence_in, n_unique)
-	y = one_hot_encode(sequence_out, n_unique)
+    # one hot encode
+    X = one_hot_encode(sequence_in, n_unique)
+    y = one_hot_encode(sequence_out, n_unique)
 
-	# reshape as 3D
-	X = X.reshape((1, X.shape[0], X.shape[1]))
-	y = y.reshape((1, y.shape[0], y.shape[1]))
+    # reshape as 3D
+    X = X.reshape((1, X.shape[0], X.shape[1]))
+    y = y.reshape((1, y.shape[0], y.shape[1]))
 
-	return (X, y)
+    return (X, y)
 
 
 X, y = get_pair(5, 2, 50)
@@ -108,22 +111,38 @@ model.add(TimeDistributed(Dense(n_features, activation='softmax')))
 print(model.summary())
 
 # compile
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam', metrics=['acc'])
 
 for epoch in range(5000):
-	X, y = get_pair(n_timesteps_in, n_timesteps_out, n_features)
-	model.fit(X, y)
+    X, y = get_pair(n_timesteps_in, n_timesteps_out, n_features)
+    model.fit(X, y)
 
 # Evaluate
 total, correct = 100, 0
 for _ in range(total):
-	X, y = get_pair(n_timesteps_in, n_timesteps_out, n_features)
-	preds = model.predict(X, verbose=0)
+    X, y = get_pair(n_timesteps_in, n_timesteps_out, n_features)
+    preds = model.predict(X, verbose=0)
 
-	print('Expected = {}, Predicted = {}'.format(one_hot_decode(y[0]), one_hot_decode(preds[0])))
+    print('Expected = {}, Predicted = {}'.format(
+        one_hot_decode(y[0]), one_hot_decode(preds[0])))
 
-	if np.array_equal(one_hot_decode(y[0]), one_hot_decode(preds[0])):
-		correct += 1
+    if np.array_equal(one_hot_decode(y[0]), one_hot_decode(preds[0])):
+        correct += 1
 
 print('Accuracy  = {}'.format(correct * 100 / total))
 
+
+class AttentionDecoder(Recurrent):
+
+    def __init__(self,
+    units,
+    output_dim,
+    activation='tanh',
+    return_probabilities=False,
+    name='AttentionDecoder',
+    kernel_initializer='glorot_uniform',
+    recurrent_initializer='orthogonal',
+    bias_initializer='zeros',
+    kernel_regularizer=None,
+    bias_regularizer=None,)
